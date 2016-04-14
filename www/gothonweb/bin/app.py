896e66ed63d56474cd1f1a1c22b23db81d2eaea3
pl <iiipod8@gmail.com>
2016-04-14 23:31:37 +0000

    2016年 04月 14日 星期四 23:31:37

--- www/gothonweb/bin/app.py
+++ www/gothonweb/bin/app.py
@@ -10,8 +10,19 @@ render = web.template.render('templates/')
 
 class Index:
     def GET(self):
-        greeting = "Hello world"
-#        return render.index(greeting = greeting)
-        return render.foo(greeting = greeting)
+        greeting = "Hello World"
+        return render.index(greeting = greeting)
+#        return render.foo(greeting = greeting)
+
+class Form:
+    def GET(self):
+        greeting = "hello, Form"
+        return render.hello_form(greeting = greeting)
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" %(form.greet, form.name)
+        return render.hello_form(greeting = greeting)
+
 if __name__ == "__main__":
     app.run()
