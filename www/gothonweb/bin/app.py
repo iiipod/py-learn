6fb27682aaf8d0dd8bbca05c5507ac2f2ac61d96
macintosh <pl@APT>
2016-04-15 21:36:49 +0800

    2016年 04月 15日 星期五 21:36:49 CST

--- www/gothonweb/bin/app.py
+++ www/gothonweb/bin/app.py
@@ -1,28 +1,27 @@
 import web
 
 urls = (
-        '/', 'Index'
+        '/hello', 'Index'
         )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
+
+#class Index:
+#    def GET(self):
+#        greeting = "Hello World"
+#        return render.index(greeting = greeting)
+##        return render.foo(greeting = greeting)
 
 class Index:
     def GET(self):
-        greeting = "Hello World"
-        return render.index(greeting = greeting)
-#        return render.foo(greeting = greeting)
-
-class Form:
-    def GET(self):
-        greeting = "hello, Form"
-        return render.hello_form(greeting = greeting)
+        return render.hello_form()
 
     def POST(self):
         form = web.input(name="Nobody", greet="Hello")
         greeting = "%s, %s" %(form.greet, form.name)
-        return render.hello_form(greeting = greeting)
+        return render.index(greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
